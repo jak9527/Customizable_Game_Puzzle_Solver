eff9034cbd5b8d8dab7c145c81d9d8d8f2eabb4a
Kelly Showers <kds1653@rit.edu>
2022-04-15 17:13:57 -0400

    Config started

--- src/puzzles/jam/model/JamConfig.java
+++ src/puzzles/jam/model/JamConfig.java
@@ -1,24 +1,45 @@
 package puzzles.jam.model;
 
+import puzzles.common.Coordinates;
 import puzzles.common.solver.Configuration;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 // TODO: implement your JamConfig for the common solver
 
-public class JamConfig {
+public class JamConfig implements Configuration{
 
-    private char[][] grid;
+    public static int rows;
+    public static int columns;
 
-    private HashMap<Character, JamCar> carMap;
+    private HashMap<Coordinates, JamCar> carMap;
+
+    private JamCar goalCar;
 
 
     public JamConfig(String filename) throws IOException{
         try (BufferedReader in = new BufferedReader(new FileReader(filename))){
 
         }
+    }
+
+    @Override
+    public Collection<Configuration> getNeighbors() {
+        ArrayList<Configuration> neighbors = new ArrayList<>();
+        for (JamCar car:carMap.){
+            carMap.
+
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isSolution() {
+        return goalCar.isSolved();
     }
 }
