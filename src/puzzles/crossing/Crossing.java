923b6480d848d6eceb143820e2a3c1a7f89ff756
Kelly Showers <kds1653@rit.edu>
2022-04-14 21:51:21 -0400

    Think all works

--- src/puzzles/crossing/Crossing.java
+++ src/puzzles/crossing/Crossing.java
@@ -1,6 +1,15 @@
 package puzzles.crossing;
 
+import puzzles.common.solver.Configuration;
 import puzzles.common.solver.Solver;
+import puzzles.strings.StringsConfig;
+
+import java.util.Collection;
+
+/**
+ * Takes a number of pups and wolves from args representing
+ * a crossing puzzle and solves it using the Solver printing the path
+ */
 
 public class Crossing {
     public static void main(String[] args) {
@@ -8,6 +17,21 @@ public class Crossing {
             System.out.println(("Usage: java Crossing pups wolves"));
         } else {
             // TODO
+            System.out.println("Pups: " + args[0] +", Wolves: " + args[1]);
+            Solver jacobCringe = new Solver();
+            Collection<Configuration> path = jacobCringe.findPath(
+                    new CrossingConfig(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+            int i = 0;
+            System.out.println("Total configs: " + jacobCringe.getTotalConfigs());
+            System.out.println("Unique configs: " + jacobCringe.getUniqueConfigs());
+            if (path == null){
+                System.out.println("No solution");
+            } else {
+                for (Configuration config : path) {
+                    System.out.println("Step " + i + ": " + config.toString());
+                    i++;
+                }
+            }
         }
     }
 }
--- src/puzzles/strings/Strings.java
+++ src/puzzles/strings/Strings.java
@@ -1,6 +1,17 @@
 package puzzles.strings;
 
+import puzzles.common.solver.Configuration;
 import puzzles.common.solver.Solver;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Takes a start and end string from args and calls upon solver to find the shortest
+ * path between the two and prints out the steps
+ *
+ * @author Kelly Showers kds1653
+ */
 
 public class Strings {
     public static void main(String[] args) {
@@ -8,6 +19,18 @@ public class Strings {
             System.out.println(("Usage: java Strings start finish"));
         } else {
             // TODO
+            System.out.println("Start: " + args[0] +", End: " + args[1]);
+            Solver jacobCringe = new Solver();
+            Collection<Configuration> path = jacobCringe.findPath(new StringsConfig(args[0], args[1]));
+            int i = 0;
+            if (path == null){
+                System.out.println("No solution");
+            } else {
+                for (Configuration config : path) {
+                    System.out.println("Step " + i + ": " + config.toString());
+                    i++;
+                }
+            }
         }
     }
 }
