b7e9be8578d1ba18de319b1981443defc1225373
Kelly Showers <kds1653@rit.edu>
2022-04-15 10:57:42 -0400

    File

--- src/puzzles/jam/model/JamConfig.java
+++ src/puzzles/jam/model/JamConfig.java
@@ -2,7 +2,18 @@ package puzzles.jam.model;
 
 import puzzles.common.solver.Configuration;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 // TODO: implement your JamConfig for the common solver
 
 public class JamConfig {
+
+
+    public JamConfig(String filename) throws IOException{
+        try (BufferedReader in = new BufferedReader(new FileReader(filename))){
+
+        }
+    }
 }
