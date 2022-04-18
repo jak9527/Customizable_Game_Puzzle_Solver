eeb7f31390a198f91cc2ba98da52787b0ba86ea2
Jacob Karvelis <jak9527@rit.edu>
2022-04-18 18:34:49 -0400

    Put in the ConsoleApplication class to use for PTUI. No idea how but like yep its there now

--- src/puzzles/hoppers/ptui/HoppersPTUI.java
+++ src/puzzles/hoppers/ptui/HoppersPTUI.java
@@ -1,10 +1,70 @@
 package puzzles.hoppers.ptui;
 
+import puzzles.common.ConsoleApplication;
 import puzzles.common.Observer;
 import puzzles.hoppers.model.HoppersModel;
 
-public class HoppersPTUI implements Observer<HoppersModel, String> {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+
+public class HoppersPTUI extends ConsoleApplication implements Observer<HoppersModel, String> {
     private HoppersModel model;
+
+    /**
+     * Used to prevent this class displaying any info before the UI
+     * has been completely set up.
+     * Scenario:
+     * <ol>
+     *     <li>This class creates the model and registers with it.</li>
+     *     <li>Model initializes itself and updates its observers.</li>
+     *     <li>
+     *         This class attempts to display information, but
+     *         {@link #start(PrintWriter)} has not yet been called,
+     *         therefore the output stream has not yet been established.
+     *     </li>
+     *     <li>Pandemonium ensues.</li>
+     * </ol>
+     */
+    private boolean initialized;
+
+    /** Where this class's messages must be sent */
+    private PrintWriter out;
+
+    /**
+     * Create the Wordle model and register this object as an observer
+     * of it. If there was a command line argument, use that as the first
+     * secret word.
+     */
+    @Override public void init() throws Exception {
+//        this.initialized = false;
+//        this.model = new HoppersModel();
+//        this.model.addObserver( this );
+//
+//        List< String > paramStrings = super.getArguments();
+//        if ( paramStrings.size() == 1 ) {
+//            final String filename = paramStrings.get( 0 );
+//            try{
+//                this.model.newGame( filename );
+//            }
+//            catch(IOException e){
+//
+//            }
+//            else {
+//                throw new Exception(
+//                        String.format(
+//                                "\"%s\" is not the required word length (%d)." +
+//                                        System.lineSeparator(),
+//                                firstWord, Model.WORD_SIZE
+//                        )
+//                );
+//            }
+//        }
+//        else {
+//            this.model.newGame();
+//        }
+    }
 
     @Override
     public void update(HoppersModel model, String msg) {
@@ -14,5 +74,10 @@ public class HoppersPTUI implements Observer<HoppersModel, String> {
         if (args.length != 1) {
             System.out.println("Usage: java HoppersPTUI filename");
         }
+    }
+
+    @Override
+    public void start(PrintWriter console) throws Exception {
+
     }
 }
